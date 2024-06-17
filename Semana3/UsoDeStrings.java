4e598f24dca9f9ba5295cc676834afac8f760a1c
Jhordy <202100363d@utea.edu.pe>
2024-06-17 09:53:41 -0500

    :3

---
+++ Semana3/UsoDeStrings.java
@@ -0,0 +1,10 @@
+package Semana3;
+public class UsoDeStrings {
+    public static void main(String[] args) {
+        String palabra = "Cerveza";
+        String frase = "¿donde esta mi cerveza?";
+
+        System.out.println("Palabra que uso con frecuencia:" +palabra );
+        System.out.println("Una fase que uso a veces:" +frase);
+    } 
+}
