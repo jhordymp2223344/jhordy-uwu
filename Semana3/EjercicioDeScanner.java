cbc3ffa36aff867a5f86d85155a60c9ee667947b
Jhordy <202100363d@utea.edu.pe>
2024-06-24 09:55:53 -0500

    trabajo

---
+++ Semana3/EjercicioDeScanner.java
@@ -0,0 +1,24 @@
+package Semana3;
+
+import java.util.Scanner;
+
+public class EjercicioDeScanner {
+    public static void main(String[] args) {
+        
+        double tasaConversion = 4.20; // 1 euro = 4.20 soles (puedes actualizar esta tasa según sea necesario)
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de euros que desea convertir a soles: ");
+        double euros = scanner.nextDouble();
+
+        double soles = euros * tasaConversion;
+
+        
+        System.out.println(euros + " euros son " + soles + " soles.");
+
+       
+        scanner.close();
+     }
+}
